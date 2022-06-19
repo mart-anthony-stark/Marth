5a48f981c65c59789f7a73a6c1f4d5a87abc016c
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-19 22:20:15 +0800

    Add: range

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,9 @@
 import Marth from "./main";
 
 const mt = new Marth();
-mt.setDataset([1,2,3])
+mt.setDataset([3, 2, 1]);
 console.log(mt.getDataset());
 console.log(mt.mean());
+console.log(mt.sort());
+console.log(mt.getDataset());
+console.log(mt.range());
